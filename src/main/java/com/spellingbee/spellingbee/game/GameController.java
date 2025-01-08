0462da2636cd881b10500d17c51501c0df71853e
ishthefish11 <ismaelparty123@gmail.com>
2025-01-08 02:19:58 -0500

    configured @CrossOrigin for controllers on localhost:5173

--- src/main/java/com/spellingbee/spellingbee/game/GameController.java
+++ src/main/java/com/spellingbee/spellingbee/game/GameController.java
@@ -3,6 +3,7 @@ package com.spellingbee.spellingbee.game;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin(origins = "http://localhost:5173")
 @RestController
 public class GameController {
     @Autowired
